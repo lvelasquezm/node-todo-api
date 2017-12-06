c6c6e47ea83ef016f6b7dcae7649d938e8ce9497
lvelasquezm <law.vzmk@gmail.com>
2017-12-06 13:39:50 -0600

    Add unit test for get todo by id endpoint

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,15 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [
-	{ text: 'First test todo' },
-	{ text: 'Second test todo' }
+	{ _id: new ObjectID(), text: 'First test todo' },
+	{ _id: new ObjectID(), text: 'Second test todo' }
 ];
 
-// Empty DB before each test is executed
+// Empty DB and insert 2 test documents before each test is executed
 beforeEach((done) => {
 	Todo.remove({}).then(() => {
 		return Todo.insertMany(todos);
@@ -66,6 +67,32 @@ describe('GET /todos', () => {
 			.expect((res) => {
 				expect(res.body.todos.length).toBe(2);
 			})
+			.end(done);
+	});
+});
+
+describe('GET /todos/:id', () => {
+	it('should return a todo', (done) => {
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.text).toBe(todos[0].text)
+			})
+			.end(done);
+	});
+
+	it('should return 404 if a todo is not found', (done) => {
+		request(app)
+			.get(`/todos/${new ObjectID().toHexString()}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return 404 for non-object ids', (done) => {
+		request(app)
+			.get('/todos/123')
+			.expect(404)
 			.end(done);
 	});
 });
