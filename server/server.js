ac7b9767286be361230b986244bcce12d8ffd82e
lvelasquezm <law.vzmk@gmail.com>
2017-12-06 13:21:44 -0600

    Add endpoint to get todo by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -25,6 +26,24 @@ app.get('/todos', (req, res) => {
 		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);
+	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	let todoId = req.params.id;
+
+	if(!ObjectID.isValid(todoId)) {
+		res.status(404).send();
+	}
+
+	Todo.findById(req.params.id).then((todo) => {
+		if(!todo) {
+			res.status(404).send();
+		}
+		
+		res.send(todo);
+	}).catch((e) => {
+		res.status(400).send();
 	});
 });
 
