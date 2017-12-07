ff7fa83b162b13ae10bd53ea6bf8b577bb3785e5
lvelasquezm <law.vzmk@gmail.com>
2017-12-07 13:56:10 -0600

    Add endpoint to update todo by id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -59,6 +60,32 @@ app.delete('/todos/:id', (req, res) => {
 			res.status(404).send();
 		}
 		
+		res.send(todo);
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
+app.patch('/todos/:id', (req, res) => {
+	let todoId = req.params.id;
+	let body = _.pick(req.body, ['text', 'completed']);
+
+	if(!ObjectID.isValid(todoId)) {
+		res.status(404).send();
+	}
+
+	if(_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(todoId, {$set: body}, {new: true}).then((todo) => {
+		if(!todo) {
+			res.status(404).send();
+		}
+
 		res.send(todo);
 	}).catch((e) => {
 		res.status(400).send();
